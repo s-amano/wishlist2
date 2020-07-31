c7ec2eba20d7e0338a75723447c193d5a456ebae
{s-amano} <{amahaya0831@gmail.com}>
2020-07-31 21:22:07 +0900

    fixed settings

--- frontend/wishlist/src/components/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const START_FETCH = 'START_FETCH'
-export const FETCH_SUCCESS = 'FETCH_SUCCESS'
-export const ERROR_CATCHED = 'ERROR_CATCHED'
-export const INPUT_EDIT = 'INPUT_EDIT'
-export const TOGGLE_MODE = 'TOGGLE_MODE'
